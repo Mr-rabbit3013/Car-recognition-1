3ecdc87876c8bb1db578408a7706b866f559e95f
Mateusz Wieczorek <wskmateusz@gmail.com>
2018-07-11 17:25:51 +0200

    Creating validation ImageDataGenerator in the same way as train one

--- keras_classificator_v1.py
+++ keras_classificator_v1.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-from keras import applications
+from keras.applications import VGG19
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
@@ -15,12 +15,6 @@ EPOCHS = 100
 
 
 def training():
-    conv_network = applications.VGG19(include_top=False, weights='imagenet', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3))
-
-    # Freeze the layers except the last 4 layers
-    for layer in conv_network.layers[:-4]:
-        layer.trainable = False
-
     train_datagen = ImageDataGenerator(
         rescale=1./255,
         rotation_range=20,
@@ -35,13 +29,25 @@ def training():
         class_mode='categorical',
         shuffle=True)
 
-    validation_datagen = ImageDataGenerator(rescale=1./255)
+    validation_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
     validation_generator = validation_datagen.flow_from_directory(
         VALIDATION_DATA_DIR,
         target_size=(IMG_WIDTH, IMG_HEIGHT),
         batch_size=BATCH_SIZE,
         class_mode='categorical',
         shuffle=True)
+
+    conv_network = VGG19(include_top=False, weights='imagenet', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3))
+
+    # Freeze the layers except the last 4 layers
+    for layer in conv_network.layers[:-4]:
+        layer.trainable = False
 
     model = Sequential()
     model.add(conv_network)
@@ -51,7 +57,6 @@ def training():
     model.add(Dense(train_generator.class_indices.items().__len__(), activation='softmax'))
     model.summary()
 
-    # Compile the model
     model.compile(loss='categorical_crossentropy',
                   optimizer=RMSprop(lr=0.0001),
                   metrics=['acc'])
@@ -59,7 +64,6 @@ def training():
     early_stopping = EarlyStopping(patience=10)
     checkpointer = ModelCheckpoint('conv_network_best.h5', verbose=1, save_best_only=True)
 
-    # Train the model
     model.fit_generator(
         train_generator,
         steps_per_epoch=train_generator.samples / train_generator.batch_size,
@@ -68,7 +72,6 @@ def training():
         validation_data=validation_generator,
         validation_steps=validation_generator.samples / validation_generator.batch_size)
 
-    # Save the model
     model.save('conv_network_final.h5')
 
 
