dd4e606ca616c6e6d14412139dabe69d6160d196
Mateusz Wieczorek <wskmateusz@gmail.com>
2018-08-02 11:20:55 +0200

    Script for training ResNet50 with include_top=false

--- keras_classificator_v2.py
+++ keras_classificator_v2.py
@@ -1,68 +1,116 @@
-import math
-import os
+from __future__ import print_function
 
-from keras import applications
+import matplotlib.pyplot as plt
+from keras import Sequential
+from keras.applications import ResNet50
+from keras.applications.resnet50 import preprocess_input
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Dense
-from keras.models import Model
+from keras.layers import Flatten, Dense, Dropout
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
-TRAIN_DIR = 'C:\\car_photos_299x299\\train'
-VALID_DIR = 'C:\\car_photos_299x299\\validation'
-IMG_SIZE = (299, 299)
-BATCH_SIZE = 4
+
+IMG_WIDTH, IMG_HEIGHT = 224, 224
+TRAIN_DATA_DIR = 'C:\\Private\\BMW\\car_photos_224x224\\train'
+VALIDATION_DATA_DIR = 'C:\\Private\\BMW\\car_photos_224x224\\validation'
+TEST_DATA_DIR = 'C:\\Private\\BMW\\car_photos_224x224\\test'
+BATCH_SIZE = 8
 EPOCHS = 100
 
-if __name__ == "__main__":
-    num_train_samples = sum([len(files) for r, d, files in os.walk(TRAIN_DIR)])
-    num_valid_samples = sum([len(files) for r, d, files in os.walk(VALID_DIR)])
 
-    num_train_steps = math.floor(num_train_samples / BATCH_SIZE)
-    num_valid_steps = math.floor(num_valid_samples / BATCH_SIZE)
+def prepare_data():
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+    train_generator = train_datagen.flow_from_directory(
+        TRAIN_DATA_DIR,
+        target_size=(IMG_WIDTH, IMG_HEIGHT),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical',
+        shuffle=True)
 
-    train_gen = ImageDataGenerator()
-    val_gen = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
+    validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    validation_generator = validation_datagen.flow_from_directory(
+        VALIDATION_DATA_DIR,
+        target_size=(IMG_WIDTH, IMG_HEIGHT),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical',
+        shuffle=True)
 
-    train_batches = train_gen.flow_from_directory(TRAIN_DIR,
-                                                  target_size=IMG_SIZE,
-                                                  class_mode='categorical',
-                                                  shuffle=True,
-                                                  batch_size=BATCH_SIZE)
-    val_batches = val_gen.flow_from_directory(VALID_DIR,
-                                              target_size=IMG_SIZE,
-                                              class_mode='categorical',
-                                              shuffle=True,
-                                              batch_size=BATCH_SIZE)
+    # test_datagen = ImageDataGenerator(rescale=1./255)
+    # test_generator = test_datagen.flow_from_directory(
+    #     TEST_DATA_DIR,
+    #     target_size=(IMG_WIDTH, IMG_HEIGHT),
+    #     batch_size=BATCH_SIZE,
+    #     class_mode='categorical',
+    #     shuffle=True)
+    # return train_generator, validation_generator, test_generator
+    return train_generator, validation_generator
 
-    model = applications.Xception(input_shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
 
-    classes = list(iter(train_batches.class_indices))
-    model.layers.pop()
-    for layer in model.layers:
+def show_history(history):
+    # list all data in history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+
+def training():
+    train_generator, validation_generator = prepare_data()
+    # train_generator, validation_generator, test_generator = prepare_data()
+
+    conv_network = ResNet50(include_top=False,
+                            weights='imagenet',
+                            input_shape=(IMG_WIDTH, IMG_HEIGHT, 3))
+
+    for layer in conv_network.layers[:-3]:
         layer.trainable = False
-    last = model.layers[-1].output
-    x = Dense(len(classes), activation="softmax")(last)
 
-    finetuned_model = Model(model.input, x)
-    finetuned_model.compile(optimizer=Adam(lr=0.0001),
-                            loss='categorical_crossentropy',
-                            metrics=['accuracy'])
-
-    for c in train_batches.class_indices:
-        classes[train_batches.class_indices[c]] = c
-    finetuned_model.classes = classes
-
-    finetuned_model.summary()
+    model = Sequential()
+    model.add(conv_network)
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(train_generator.class_indices.items().__len__(), activation='softmax'))
     model.summary()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=0.001),
+                  metrics=['accuracy'])
 
     early_stopping = EarlyStopping(patience=10)
-    checkpointer = ModelCheckpoint('Xception_best.h5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath='car_resNet50_best.h5', verbose=0, save_best_only=True)
 
-    finetuned_model.fit_generator(train_batches,
-                                  steps_per_epoch=num_train_steps,
-                                  epochs=EPOCHS,
-                                  callbacks=[early_stopping, checkpointer],
-                                  validation_data=val_batches,
-                                  validation_steps=num_valid_steps)
-    finetuned_model.save('Xception_final.h5')
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=train_generator.samples / train_generator.batch_size,
+        epochs=EPOCHS,
+        callbacks=[early_stopping, checkpointer],
+        validation_data=validation_generator,
+        validation_steps=validation_generator.samples / validation_generator.batch_size)
+
+    # evaluate_scores = model.evaluate_generator(generator=test_generator)
+
+    model.save('car_resNet50_final.h5')
+    show_history(history)
+
+
+if __name__ == '__main__':
+    training()
