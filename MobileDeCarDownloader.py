d0ff2d46e0ed868197d51dc257d574f8a2b5dbb0
Mateusz Wieczorek <wskmateusz@gmail.com>
2018-11-13 19:38:29 +0100

    fix - downloading photos from mobile.de

--- MobileDeCarDownloader.py
+++ MobileDeCarDownloader.py
@@ -21,81 +21,83 @@ MAIN_URL = 'https://www.mobile.de'
 PAGE_NUMBER_PLACEHOLDER = '%%%'
 PAGE_NUMBER_PHRASE = 'pgn:'
 
-f32_URL = 'https://www.mobile.de/pl/samochod/bmw-seria-4/vhc:car,ms1:3500_-53_,vcg:sportscar'
-
 
 class CarDownloader:
-	"""Class for downloading images from Mobile.de"""
+    """Class for downloading images from Mobile.de"""
 
-	def __init__(self):
-		self
+    def __init__(self):
+        self
 
-	def download_images(self, url, directory):
-		car_counter = 1
-		car_pager = 1
-		while True:
-			correct_url = str(url).replace(PAGE_NUMBER_PLACEHOLDER, PAGE_NUMBER_PHRASE + str(car_pager))
-			car_pager += 1
-			webpage_source = read_html_page_source(correct_url)
-			if check_page(webpage_source):
-				car_page_fragments = get_all_car_page_fragments(webpage_source)
-				car_links = get_all_car_pages(car_page_fragments)
-				for car_link in car_links:
-					log.info('Downloading photos from car number: ' + str(car_counter))
-					car_counter += 1
-					subpage = read_html_page_source(car_link)
-					photo_links = get_all_car_photo_links_from(subpage)
-					for photo_url in photo_links:
-						save_file_on_disc_from(photo_url, directory)
+    def download_images(self, url, directory):
+        car_counter = 1
+        car_pager = 1
+        while True:
+            correct_url = str(url).replace(PAGE_NUMBER_PLACEHOLDER, PAGE_NUMBER_PHRASE + str(car_pager))
+            car_pager += 1
+            webpage_source = read_html_page_source(correct_url)
+            if check_page(webpage_source):
+                car_page_fragments = get_all_car_page_fragments(webpage_source)
+                car_links = get_all_car_pages(car_page_fragments)
+                for car_link in car_links:
+                    log.info('Downloading photos from car number: ' + str(car_counter))
+                    car_counter += 1
+                    subpage = read_html_page_source(car_link)
+                    photo_links = get_all_car_photo_links_from(subpage)
+                    for photo_url in photo_links:
+                        save_file_on_disc_from(photo_url, directory)
 
 
 def get_all_car_photo_links_from(webpage_source):
-	images = re.findall(r"<div class=\"js-image-data\" data-title(.+?)<\/div>", webpage_source)[0]
-	links = re.findall(r"https:(.+?).JPG", images)
-	update_pages = ["https:" + link + ".JPG" for link in links]
-	return update_pages
+    images = re.compile(u"<div class=\"js-image-data\" data-title(.+?)<\/div>", re.UNICODE)
+    images = images.findall(webpage_source)[0]
+    links = re.compile(u"https:(.+?).JPG", re.UNICODE)
+    links = links.findall(images)
+    update_pages = ['https:' + link + '.JPG' for link in links]
+    return update_pages
 
 
 def get_all_car_page_fragments(webpage_source):
-	return re.findall(r"<article class(.+?)<\/article>", webpage_source)
+    return re.findall(r"<article class(.+?)<\/article>", webpage_source)
 
 
 def get_all_car_pages(car_fragments):
-	links = set()
-	for fragment in car_fragments:
-		links.add(MAIN_URL + (re.findall(r"href=\"(.+?)\">", fragment)[0]))
-	return links
+    links = set()
+    for fragment in car_fragments:
+        links.add(MAIN_URL + (re.findall(r"href=\"(.+?)\">", fragment)[0]))
+    return links
 
 
 def check_page(webpage_source):
-	ERROR_MESSAGE_PREFIX = '<p class="js-error-message u-text-center">'
-	if ERROR_MESSAGE_PREFIX not in webpage_source:
-		return True
-	else:
-		return False
+    ERROR_MESSAGE_PREFIX = '<p class="js-error-message u-text-center">'
+    if ERROR_MESSAGE_PREFIX not in webpage_source:
+        return True
+    else:
+        return False
 
 
 def read_html_page_source(url):
-	ua = UserAgent(cache=False)
-	cookie = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-			  'accept-encoding': 'gzip, deflate, br',
-			  'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6',
-			  'cookie': 'sorting_e=""; show_qs_e=vhc%3Acar%2Cms1%3A3500_-53_; vi=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaWQiOiI2MThiYzBlNy0wNzAwLTRiYTMtOGQ2OS1mMmZkMDQ1Y2M3MTgiLCJhdWQiOltdLCJpYXQiOjE1NDE1NzYzMzR9.6W3mN_C5U2gbXkSFbWm99XVwiTnsWwJuZiADHNIYhZg; _ga=GA1.2.495630715.1541576336; optimizelyEndUserId=oeu1541576335833r0.33012600410861226; visited=1; axd=1001316771521130073; mdeConsentData=BOW2eGqOW2eGqB9ABBDEBa-AAAJw-BOgACAAkABgA1gBuAL8AYQBigDIAM0AaABpgDYAOcAdwB4AD1AHwAhoBEAEjAJMApYBTgFXAK0Ar4BYgFpALYAuYBeAGXAM0AzoBoAG0AN2Ab4B0QDqAPaAfACFQEMAQ6AiACNQEcAR2Aj4CRwElASeAlICVwEtATCAmgCcQE6AUEAocBRQFGgKQApYBTYCngKrAVcBW4CugK-AWKAsoC1QFtAW6AuAC4gFzgLqAvUBfAF-gMAAwMBgwGIgMUAxcBjQGOAMhAZIBlYDLgM1AZwBnYDPgNAAaKA0oDXwGyAbQA2wBuIDdAN5Ab8BwgDiQHGAceA5QDlwHNAc6A6ADqwHZAdwA7wB4ADyAHmgPQA9YB7YD3gPjAfMB9AD7CcP; mdeCustomConsentData=25; _abck=D40AE9EB20F0D4EC1D82D077F4152725685E64373E3100008F96E25B8A016433~0~pJGUz23KSDuaXNklYhrNtDKX8LcA99Pjwc70oDhLBS8=~-1~-1; via=google; mobile.LOCALE=en; bm_sz=D9EF162DD3C32C8097F2202B783B75C9~QAAQLmReaMQV4OFmAQAA2Z+5CK7/IvIu7KnGghMAARjdvrDsSXO6+EBcjcdyTL9Qz5iWOA2+uscHRDKh30fHRWL6/RtOAfLmQ6GyoXUajI7L8KVKsMMmC5PgCXfWmBGTQFrAo8J2c9Mq0pAtiqzL1cJW/caGG4Te8QrHm5a4VA//QJFjcMFQXaAEXzarJw==; _gid=GA1.2.777159791.1542039644; ak_bmsc=82BB32FCE739F677698F5B5698E8B863685E642EB80500005AA8E95BE0026011~pll+Y0cnvz1jtXl47g5JNnUAt2Z/QRDCOXZiLpDJ1RKxfEP8Iz2TC49gxg0OvbXV4u9iTbrwkiet5CxG5kxUS3t6DgoSeErMy33a5aVoMndPgwq9ODz1gSRfb7eWgauJAMBxsGX2UQ9HamKWCkpkfnzXZvShWFjQMX4h5vWJWKpIOY6uPc6QLO91VK0Wlo3lGLJf+lcOg4ynxYv8iPIlHKgNtIjzidxbpe5IhSWVJH44JwEa6RcidPDUET1aBtwMRB; tis=EP117%3A1891%7CEP117%3A1891; _gat=1; bm_sv=73A262F052614595DFA94D531D14DCF1~GLs/UWOsaegwN1ckoFwmIvbO95bwHjo6/CARviHeTzJLafZewk7sJOz1cWvLXC00MAphEHsXvBIOlao6Pt8eBV1OX90XT13Z3n7bOEhL+mvUX+C9OSSLj9VdukMZBKmVmMFsCsO+FQC5+/nMfRXYPxRNWRZtvEsyzOtmqyhnhFE=',
-			  'upgrade-insecure-requests': '1',
-			  'User-Agent': str(ua.chrome)}
-	response = requests.get(url, cookies=cookie)
-	return str(response.content)
+    ua = UserAgent()
+    cookie = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+              'accept-encoding': 'gzip, deflate, br',
+              'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6',
+              'cookie': 'sorting_e=""; show_qs_e=vhc%3Acar%2Cms1%3A3500_-53_; mobile.LOCALE=de; vi=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaWQiOiJkM2NjODEwZC1lMjY0LTQyM2ItYmQ0OC0yMTczODZkNzUyZmYiLCJhdWQiOltdLCJpYXQiOjE1NDE1Mzg1MDB9.lY_yHXmzxPDWAnsdHVqSnyfhHpaTn03-J9W-KdCbwIE; _ga=GA1.2.2040238419.1541538502; __gads=ID=03706eb646499294:T=1541538501:S=ALNI_MbLP8npOqdXGb3FBKZ7eWw6mDcLNA; optimizelyEndUserId=oeu1541538504530r0.5612834202323105; visited=1; _abck=3C05924E088668DEE73C9D69CED42758685E64373E310000C402E25B3C43A239~0~uNZrWf7ouRh1FdBfnOvs4EIdGxtj8EJbN09rS257yco=~-1~-1; axd=1002018852208240172; cto_axid=HJwid2y5JJ3c_LtJdklIHl34dFtyxT_v; mdeConsentData=BOW1BvmOW1BvmB9ABBDEBa-AAAJw-BOgACAAkABgA1gBuAL8AYQBigDIAM0AaABpgDYAOcAdwB4AD1AHwAhoBEAEjAJMApYBTgFXAK0Ar4BYgFpALYAuYBeAGXAM0AzoBoAG0AN2Ab4B0QDqAPaAfACFQEMAQ6AiACNQEcAR2Aj4CRwElASeAlICVwEtATCAmgCcQE6AUEAocBRQFGgKQApYBTYCngKrAVcBW4CugK-AWKAsoC1QFtAW6AuAC4gFzgLqAvUBfAF-gMAAwMBgwGIgMUAxcBjQGOAMhAZIBlYDLgM1AZwBnYDPgNAAaKA0oDXwGyAbQA2wBuIDdAN5Ab8BwgDiQHGAceA5QDlwHNAc6A6ADqwHZAdwA7wB4ADyAHmgPQA9YB7YD3gPjAfMB9AD7CcP; mdeCustomConsentData=25; GED_PLAYLIST_ACTIVITY=W3sidSI6IlBFL1QiLCJ0c2wiOjE1NDE1MzkzMDcsIm52IjoxLCJ1cHQiOjE1NDE1MzkzMDYsImx0IjoxNTQxNTM5MzA2fV0.; bm_sz=7ADFEF820ADF0B581A6E846D98BDE950~QAAQN2ReaOoFrdJmAQAAErnQDVY35dMknLbMy3tltWW8W5WPFg3i6a+bnmyGiLnwxy/Jz9ZS7PmMbTH953mxSArLyApCdSSFiEODMxkcC8Y0wJh8mYXJIQg5duwL16N2XJMKRcWUhfPiQiD6MY7XR773zulKPdDUDVuudFBfJcgGBaenTaQUjBgZLWVLxQ==; _gid=GA1.2.1081161282.1542125045; ak_bmsc=623CEF43BB554C8FDFB1E105B3145704685E642EB80500000915EB5B4505672C~plCR1b79+SyTql/Hgv5HE8QI7p2/f+8T39cafcgAqd8MXMsi/3yvx6Ip2wy2PJ46ajyxjaPOFYnSbpQEuVtqUcpsb0y82VSk/zHeFroX3lNMiKRrr468U6Ri0YvydAcV8ZoZyYhrx1NQrCu7mqMkCjk4M321vEH+OMPN9sbf33qw2MfmPCvQ8dhDNVjsUE3WDscRJuJqfgmwOfVugj9l8Ix9SBXE9BJc++BxqUdJQEsp74QktCTd3zEOCBpTI7H5bu; bm_sv=F66A3D9C1589BDF34F6D513E863C4D96~GLs/UWOsaegwN1ckoFwmIgv/aE2KbEdCOJpiNO1fiD88V7BFT4JALlDkQjSazHO9UyqnfwZDiB8cuh8Qp6IN5k/UJfIkqdy6Zwd+kWNYNGbZgr/uux2aBlzkGuCRRLUCsOOilnwNQTkY25+hJJ6P8081pQAMDAa1Z09xk6E0sWI=',
+              'upgrade-insecure-requests': '1',
+              'User-Agent': str(ua.chrome)}
+    response = requests.get(url, cookies=cookie)
+    return str(response.text)
 
 
 def write_to_file(file, collection):
-	with open(file, 'w') as f:
-		for item in collection:
-			f.write("%s\n" % item)
+    with open(file, 'w') as f:
+        for item in collection:
+            f.write("%s\n" % item)
 
 
 def save_file_on_disc_from(url, directory):
-	try:
-		testfile = URLopener()
-		testfile.retrieve(url, directory + str(url).split("00")[1].replace("/", ""))
-	except Exception as exception:
-		log.error('Unexpected exception: ' + str(exception))
+    try:
+        testfile = URLopener()
+        url = str(url).replace("\\u003d", '=')
+        filename = directory + str(url).split("00")[1].replace("/", "")
+        testfile.retrieve(url, filename)
+    except Exception as exception:
+        log.error('Unexpected exception: ' + str(exception))
--- car.py
+++ car.py
@@ -1,4 +1,5 @@
 import CarDownloader
+import MobileDeCarDownloader
 
 e60_URL = 'https://www.otomoto.pl/osobowe/bmw/seria-5/e60-2003-2010/-/sedan/?search%5Bfilter_enum_damaged%5D=0&search%5Bbrand_program_id%5D%5B0%5D=&search%5Bcountry%5D=%3D%24page%3D2&page='
 e90_URL = 'https://www.otomoto.pl/osobowe/bmw/seria-3/e90-2005-2012/-/sedan/?search%5Bfilter_float_year%3Ato%5D=2008&search%5Bfilter_enum_damaged%5D=0&search%5Bbrand_program_id%5D%5B0%5D=&search%5Bcountry%5D=%3D&page='
@@ -13,8 +14,11 @@ e83_URL = 'https://www.otomoto.pl/osobowe/bmw/x3/e83-2003-2010/?search%5Bfilter_
 f15_URL = 'https://www.otomoto.pl/osobowe/bmw/x5/f15-2013/?search%5Bfilter_float_year%3Ato%5D=2016&search%5Bfilter_enum_damaged%5D=0&search%5Bcountry%5D=%3D&page='
 e70_URL = 'https://www.otomoto.pl/osobowe/bmw/x5/e70-2006-2013/?search%5Bfilter_enum_damaged%5D=0&search%5Bcountry%5D=%3D&page='
 
-CAR_PHOTOS_DIRECTORY = 'C:\\Private\\BMW\\e70\\unknown\\'
+# replace pgn: with %%%
+f32_URL = 'https://www.mobile.de/pl/samochod/bmw-seria-4/vhc:car,pgn:%%%,pgs:50,ms1:3500_-53_,dmg:false,vcg:sportscar'
+
+CAR_PHOTOS_DIRECTORY = 'C:\\Users\\Mateusz\\Desktop\\f32\\'
 
 if __name__ == '__main__':
-    downloader = CarDownloader.CarDownloader()
-    downloader.download_images(e70_URL, CAR_PHOTOS_DIRECTORY)
+    downloader = MobileDeCarDownloader.CarDownloader()
+    downloader.download_images(f32_URL, CAR_PHOTOS_DIRECTORY)
